6befa00e1f99e3c5007ca25f1dba17c033bc25b3
Victor430 <39733624+Victor430@users.noreply.github.com>
2020-12-03 22:31:15 -0500

    Add files via upload

---
+++ Optimization.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 30 20:50:05 2020
+
+@author: Chenwei Wang
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cal_return(df):
+    """
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        A DataFrame of stock prices, using dates as index, tickers as columns, 
+        prices are Adj Close.
+
+    Returns
+    -------
+    A DataFrame of annualized stock returns.
+
+    """
+    
+    stock_returns = np.log(df).diff().dropna()*250
+    return stock_returns
+
+def plot_data(df):
+    """
+
+    Parameters
+    ----------
+    stock_returns : Pandas DataFrame
+        A DataFrame of stock prices or returns, using dates as index, 
+        tickers as columns.
+
+    Returns
+    -------
+    Plot the data, whether in prices or returns, to view data anomalies.
+    There are 10 columns in df by default..
+
+    """
+    # layout specifies that the plots will listed in 4 rows and 3 columns.
+    df.plot(subplots=True, layout=(4, 3), figsize=(12, 9))
+
+def cal_corr(stock_returns):
+    """
+
+    Parameters
+    ----------
+    stock_returns : Pandas DataFrame
+        A DataFrame of stock returns, using dates as index, 
+        tickers as columns.
+
+    Returns
+    -------
+    A correlation matrix.
+
+    """
+    stock_corr = stock_returns.corr()
+    return stock_corr
+
+def Makowitz_weights(mu, sigma, a):
+    """
+
+    Parameters
+    ----------
+    mu : Numpy array in n degrees
+        Vector of means.
+    sigma : n * n Numpy array
+        Var-cov matrix.
+    a : Numpy array
+        Required returns.
+
+    Returns
+    -------
+    Makowitz weights and the portfolio variance.
+
+    """
+    n = len(mu) # number of assets
+    Smu = np.linalg.solve(sigma, mu) # inv(sigma)*mu
+    Siota = np.linalg.solve(sigma, np.ones(n)) # inv(sigma)*iota
+    
+    A = Smu.sum()
+    B = np.dot(mu, Smu)
+    C = Siota.sum()
+    D = B*C-A*A
+    
+    w = (B*Siota-A*Smu)/D+(C*Smu-A*Siota)/D*a
+    portfolio_var = np.dot(w, sigma @ w)
+    return w, portfolio_var
+
+def efficient_frontier(stock_returns):
+    """
+
+    Parameters
+    ----------
+    stock_returns : Pandas DataFrame
+        A DataFrame of stock returns, using dates as index, 
+        tickers as columns.
+
+    Returns
+    -------
+    Different required returns, together with portfolio variances.
+
+    """
+    mu = stock_returns.mean().values
+    sigma = stock_returns.cov().values
+    
+    num_grid_points = 100
+    a = np.linspace(mu.min(), mu.max(), num_grid_points)
+    portfolio_var = np.zeros(num_grid_points)
+    
+    for i in range(num_grid_points):
+        w, portfolio_var[i] = Makowitz_weights(mu, sigma, a[i])
+    
+    return a, portfolio_var
+
+def plot_efficient_frontier(stock_returns):
+    """
+
+    Parameters
+    ----------
+    stock_returns : Pandas DataFrame
+        A DataFrame of stock stock returns, using dates as index, 
+        tickers as columns. One can also take slices of it in order to realize
+        stock selection.
+
+    Returns
+    -------
+    None. Showing plots of efficient frontier.
+
+    """
+    a1, portfolio_var_1 = efficient_frontier(stock_returns)
+    # a2, portfolio_var_2 = efficient_frontier(stock_returns_2)
+    plt.figure()
+    plt.plot(np.sqrt(portfolio_var_1), a1, label="Ten stocks")
+    # plt.plot(np.sqrt(portfolio_var_2), a2, label="Selected stocks")
+    plt.legend() # Showing which plot belongs to which model
+    plt.show()
+
+
+
+
+    
