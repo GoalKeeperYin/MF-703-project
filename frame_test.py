90dc7b3451d4d7a046063ca580c14a149127be85
yin <274262027@qq.com>
2020-12-06 01:47:00 +0800

    Update frame_test.py
    Add two functions:prediction,weight(not tested with real data yet)
    Next several steps: 1 how to buy stocks given the total money we hold and optimal weights 2 how to manage data between the class methods "prediction" "weight_of_prediction" and  "next" 3 how to buy in the class methods "buy"

--- frame_test.py
+++ frame_test.py
@@ -1,6 +1,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from Optimization import *
+
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
@@ -8,20 +10,45 @@ import sys  # To find out the script name (in argv[0])
 # Import the backtrader platform
 import backtrader as bt
 
-class TestStrategy(bt.Strategy):
 
-    def select_stock(self,dataframe):
+class TestStrategy(bt.Strategy):
+    def prediction(self, stock_name, start_date, end_date, predict_date):
+        """
+
+        :param stock_name: every stock name
+        :param start_date: start data of input for model
+        :param end_date:  end date of input for model
+        :param predict_date: tell model to predict which date
+        :return: the predicted return
+        """
+        predicted_return = 0
+        return predicted_return
+
+    def select_stock(self, dataframe):
         '''
         # using models to calculate the n stocks with highest return
         :param dataframe: all stocks with X days
         :return: a list of return sorted (id and name)
         '''
-    def optimization(self,stocks_history_data):
+
+    def weight_of_portfolio(self, stock_index_list,start_date,end_date):
         '''
 
-        :param stocks_history_data:
-        :return: weight of selected stocks
+        :param stock_index_list: which stock to select
+        :return: the weight of everystock
         '''
+        selected_stock_df = list()
+        for stock_index in stock_index_list:
+            stock_data = pd.read_csv(self.stock_list[stock_index])['Adj Close'].loc[start_date:end_date]
+            selected_stock_df.append(stock_data)
+        price_df = pd.concat(selected_stock_df, axis=1)
+        '''
+        price.columns = ['XLB', 'XLE', 'XLF']
+        '''
+        stock_return = cal_return(price)
+        weight,variance = find_optimal(stock_return)
+
+        return weight
 
     def log(self, txt, dt=None):
         ''' Logging function fot this strategy'''
@@ -39,19 +66,27 @@ class TestStrategy(bt.Strategy):
 
         """
         self.size = list()
-        self.stock_list = list()
+        # filename:["XLB.csv","SPY.csv","XLF.csv"] contains all files to be used
+        self.stock_list = ["XLB.csv", "SPY.csv", "XLF.csv"]
+        # prediction
+        self.next_return_list = list()
+        for stock in self.stock_list:
+            next_return = self.prediction(stock)
+            self.next_return_list.append(next_return)
+        self.stock_index_list = self.select_stock(self.next_return_list)
+        weight_of_stocks = self.weight_of_portfolio(self.stock_index_list)
 
-
-
+        # the strategy of buying stocks given the total money we hold and optimal weights
+        # how to manage data between the class methods "prediction" "weight_of_prediction"
+        # how to buy in the class methods "buy"
         self.dataclose = list()
         for i in range(2):
-
-            self.dataclose .append(self.datas[0].close)
+            self.dataclose.append(self.datas[0].close)
             self.dataclose.append(self.datas[1].close)
             self.dataclose.append(self.datas[2].close)
+
     def next(self):
         # Simply log the closing price of the series from the reference
-
 
         for i, d in enumerate(self.datas):
 
@@ -60,9 +95,10 @@ class TestStrategy(bt.Strategy):
             pos = self.getposition(d).size
             if not pos:  # no market / no orders
                 self.buy(data=d, size=1000)
-                print('buy',d._name)
+                print('buy', d._name)
             else:
-                print('hold',d._name,self.getposition(d).size)
+                print('hold', d._name, self.getposition(d).size)
+
 
 if __name__ == '__main__':
     # Create a cerebro entity
@@ -90,8 +126,6 @@ if __name__ == '__main__':
             todate=datetime.datetime(2012, 12, 31),
             reverse=False)
         cerebro.adddata(data, name=datalist[i][1])
-
-
 
     # Set our desired cash start
     cerebro.broker.setcash(10000000.0)
